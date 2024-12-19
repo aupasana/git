bee6a7646a9d72132f5904c6d6c69512be930a8e
Ajit Krishnan <aupasana@users.noreply.github.com>
2024-12-18 17:52:01 -0800

    gita - remove trailing spaces

--- format_lines.sh
+++ format_lines.sh
@@ -3,11 +3,11 @@
 echo Stripping trailing spaces
 perl -i -pe 'BEGIN{undef $/;} s/ *\n/\n/smg' ${1}
 
-# echo Adding newline on shloka boundaries
-# perl -i -pe 'BEGIN{undef $/;} s/([॥-])\n([^\n])/$1\n$2/smg' ${1}
+#echo Adding newline on shloka boundaries
+#perl -i -pe 'BEGIN{undef $/;} s/([॥-])\n([^\n])/$1\n\n$2/smg' ${1}
 
-echo Adding trailing spaces after any non-empty line, which is not followed by a blank line
-perl -i -pe 'BEGIN{undef $/;} s/([^\n])\n/$1  \n/smg' ${1}
+#echo Adding trailing spaces after any non-empty line, which is not followed by a blank line
+#perl -i -pe 'BEGIN{undef $/;} s/([^\n])\n/$1  \n/smg' ${1}
 
 #echo Adding trailing spaces after EOL puncuation
 #perl -i -pe 'BEGIN{undef $/;} s/([-।॥,])\n([^\n])/$1  \n$2/smg' ${1}
