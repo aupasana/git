b8faddf5bc2e2398f01c4be730b1fe54b750ae9c
Ajit Krishnan <aupasana@users.noreply.github.com>
2024-12-18 17:53:34 -0800

    gita - separate shlokas

--- format_lines.sh
+++ format_lines.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-echo Stripping trailing spaces
-perl -i -pe 'BEGIN{undef $/;} s/ *\n/\n/smg' ${1}
+#echo Stripping trailing spaces
+#perl -i -pe 'BEGIN{undef $/;} s/ *\n/\n/smg' ${1}
 
-#echo Adding newline on shloka boundaries
-#perl -i -pe 'BEGIN{undef $/;} s/([॥-])\n([^\n])/$1\n\n$2/smg' ${1}
+echo Adding newline on shloka boundaries
+perl -i -pe 'BEGIN{undef $/;} s/॥\n([^\n])/॥\n\n$1/smg' ${1}
 
 #echo Adding trailing spaces after any non-empty line, which is not followed by a blank line
 #perl -i -pe 'BEGIN{undef $/;} s/([^\n])\n/$1  \n/smg' ${1}
