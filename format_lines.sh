b371fd2b1e1801defab554448d88469cc27248a6
Ajit Krishnan <aupasana@users.noreply.github.com>
2024-12-20 07:46:52 -0800

    updates

--- format_lines.sh
+++ format_lines.sh
@@ -9,6 +9,11 @@ perl -i -pe 'BEGIN{undef $/;} s/ *\n/\n/smg' ${1}
 echo Adding trailing spaces after any non-empty line, which is not followed by a blank line
 perl -i -pe 'BEGIN{undef $/;} s/([^\n])\n/$1  \n/smg' ${1}
 
+# perl -i -pe 'BEGIN{undef $/;} s/([^।॥]+)\n/$1।\n/smg' suktas/rudra-krama.md
+# perl -i -pe 'BEGIN{undef $/;} s/\n।\n/\n\n/smg' suktas/rudra-krama.md
+# perl -i -pe 'BEGIN{undef $/;} s/ ।\n/।/smg' suktas/rudra-krama.md
+
+
 echo Remove spaces before . and ..
 perl -i -pe 's/ ।/।/g' ${1}
 perl -i -pe 's/ ॥/॥/g' ${1}
--- format_svaras.sh
+++ format_svaras.sh
@@ -17,6 +17,9 @@ perl -i -pe 's//॑/g' ${1}
 # non-standard gum
 perl -i -pe 's//ꣳ/g' ${1}
 
+# non-standard H
+perl -i -pe 's//ः' ${1}
+
 ####
 ####  Format .gm and .gg properly
 ####
@@ -34,6 +37,13 @@ perl -i -pe 's/॒ꣴ/॒ꣴ॒/g' ${1}
 perl -i -pe 's/᳚ꣴ/ꣴ᳚/g' ${1}
 
 perl -i -pe 's/ँ/ꣳ/g' ${1}
+
+####
+#### Format H properly
+
+perl -i -pe 's/॑ः/ः॑/g' ${1}
+perl -i -pe 's/।/।/g' {1}
+####
 
 ####
 #### Don't show properly on kindle
