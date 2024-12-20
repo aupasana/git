c0a24fc769dc87d99029e5fd9aeadffe6b7c4913
Ajit Krishnan <aupasana@users.noreply.github.com>
2024-12-20 07:59:10 -0800

    updates

--- create-svaras.sh
+++ create-svaras.sh
@@ -1,5 +1,16 @@
 #!/bin/zsh
 
+file_base=${1:r}
+file_base=${file_base:t}
+
 cp ${1} tmp/${1:t}
 ./format_svaras.sh tmp/${1:t}
-./create.sh tmp/${1:t}
+./format_lines.sh tmp/${1:t}
+
+pdf_params="--paper-size=a5 --pdf-page-margin-left=36 --pdf-page-margin-right=36 --pdf-page-margin-top=36 --pdf-page-margin-bottom=36 --pdf-page-numbers"
+
+echo Creating devanagari files
+pandoc tmp/${file_base}.md -o epub/${file_base}-devanagari.epub
+
+echo Converting to pdf
+ebook-convert epub/${file_base}-devanagari.epub pdf/${file_base}-devanagari.pdf $=pdf_params 
--- format_svaras.sh
+++ format_svaras.sh
@@ -16,6 +16,7 @@ perl -i -pe 's//॑/g' ${1}
 
 # non-standard gum
 perl -i -pe 's//ꣳ/g' ${1}
+perl -i -pe 's//ꣳ/g' ${1}
 
 # non-standard H
 perl -i -pe 's//ः' ${1}
